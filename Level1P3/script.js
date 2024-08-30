44695ec23751e698b8463602938803ad2233b8ce
Alka Gautam <118168669+Akku7388@users.noreply.github.com>
2024-08-30 22:39:12 +0530

    Add files via upload

---
+++ Level1P3/script.js
@@ -0,0 +1,58 @@
+let currentInput = "";
+let operator = "";
+let previousInput = "";
+
+function appendNumber(number) {
+  currentInput += number;
+  updateDisplay();
+}
+
+function setOperation(op) {
+  if (currentInput === "") return;
+  if (previousInput !== "") {
+    calculate();
+  }
+  operator = op;
+  previousInput = currentInput;
+  currentInput = "";
+}
+
+function calculate() {
+  let result;
+  const prev = parseFloat(previousInput);
+  const current = parseFloat(currentInput);
+  if (isNaN(prev) || isNaN(current)) return;
+
+  switch (operator) {
+    case "+":
+      result = prev + current;
+      break;
+    case "-":
+      result = prev - current;
+      break;
+    case "*":
+      result = prev * current;
+      break;
+    case "/":
+      result = prev / current;
+      break;
+    default:
+      return;
+  }
+
+  currentInput = result;
+  operator = "";
+  previousInput = "";
+  updateDisplay();
+}
+
+function clearDisplay() {
+  currentInput = "";
+  operator = "";
+  previousInput = "";
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById("display").value = currentInput;
+}
